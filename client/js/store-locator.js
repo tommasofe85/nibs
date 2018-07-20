899d33a3f7b613703edcb353884be64f2e157109
tommasofe85 <38831845+tommasofe85@users.noreply.github.com>
2018-07-20 10:19:55 +0200

    Update store-locator.js

--- client/js/store-locator.js
+++ client/js/store-locator.js
@@ -35,10 +35,10 @@ angular.module('nibs.store-locator', [])
             icon = L.icon({
                 iconUrl: 'img/leaf-green.png',
                 shadowUrl: 'img/leaf-shadow.png',
-                iconSize:     [38, 95], // size of the icon
-                shadowSize:   [50, 64], // size of the shadow
+                iconSize:     [38, 38], // size of the icon
+                shadowSize:   [0, 0], // size of the shadow
                 iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-                shadowAnchor: [4, 62],  // the same for the shadow
+                shadowAnchor: [0, 0],  // the same for the shadow
                 popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
             });
 
